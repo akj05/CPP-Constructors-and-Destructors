55d5b2574e5c6a3ef31aefe1e0aaecde55c88d94
Ankush Kumar Jha <jha.akj05@gmail.com>
2025-09-02 19:42:47 +0530

    Constructor outside the Class.cpp

---
+++ Constructor outside the Class.cpp
@@ -0,0 +1,39 @@
+// Ankush Jha
+// 24070123016
+
+#include <iostream>
+using namespace std;
+
+class Student {
+    string name;
+    double fee;
+
+public:
+    Student();
+    void display();
+};
+
+Student::Student() {
+    cout << "Enter the name of Student: ";
+    cin >> name;
+    cout << "Enter the fee: ";
+    cin >> fee;
+}
+
+void Student::display() {
+    cout << "\nStudent Name: " << name;
+    cout << "\nFee: " << fee << endl;
+}
+
+int main() {
+    Student s;
+    s.display();
+    return 0;
+}
+
+// OUTPUT
+// Enter the name of Student: Ankush Jha
+// Enter the fee: 156000
+
+// Student Name: Ankush Jha
+// Fee: 156000
